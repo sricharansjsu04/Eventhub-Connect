1ac99580a4860d230d98d98698842487de4b6371
AshishVeda <vedaashish2@gmail.com>
2023-11-16 21:41:15 -0800

    Added event creation backend code

---
+++ backend/eventCreation/config/mySql.js
@@ -0,0 +1,11 @@
+const config = {
+    db:{
+        host: "clouddb.c9yc7iqlqitw.us-east-2.rds.amazonaws.com",
+        user: "root",
+        password: "Lumberjack98",
+        port: 3306,
+        database: "PlaypalDB"
+    }
+}
+
+module.exports = config;
---
+++ backend/eventCreation/controllers/dbConnect.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql2');
+var config = require("../config/mySql")
+
+var connection = mysql.createConnection(config.db);
+
+
+// connection.on('error', function(err) {
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//         console.log('Reconnecting...');
+//         connection = mysql.createConnection(config.db);
+//         connection.connect();
+//     } else {
+//         throw err;
+//     }
+// });
+
+module.exports =  connection;
