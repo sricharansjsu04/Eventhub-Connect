afffae3d20f5edc85c7bf63a8b114c7ad73aa6b8
Shireesh Vennamaneni <shireesh@Shireeshs-MacBook-Air.local>
2023-12-06 19:42:29 -0800

    Changed Frontend to Backend call from ALB to api.playpal.live

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
     getPlayAreas: 'https://pbh79m29ck.execute-api.us-east-2.amazonaws.com/api/getPlayAreas',
-    registerUser: 'https://eventuseralb-1046506387.us-east-2.elb.amazonaws.com/users/register',
-    updateStatusBaseUrl: 'https://eventuseralb-1046506387.us-east-2.elb.amazonaws.com' 
+    registerUser: 'https://api.playpal.live/users/register',
+    updateStatusBaseUrl: 'https://api.playpal.live' 
   };
   
   export default apiConfig;
