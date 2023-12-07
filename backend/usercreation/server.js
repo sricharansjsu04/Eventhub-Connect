3838ef938edff1ad428ab41d4707854dbe296da4
Shireesh Vennamaneni <shireesh@Shireeshs-MacBook-Air.local>
2023-12-06 18:48:46 -0800

    removed 2 lines from server.js as it was causing issues

--- backend/usercreation/server.js
+++ backend/usercreation/server.js
@@ -1,10 +1,3 @@
-const envConfig = require('dotenv').config();
-
-if (envConfig.error) {
-  throw envConfig.error;
-}
-
-console.log(envConfig.parsed);
 const app = require('./app');
 
 const PORT =  3001;
